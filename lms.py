ebfe96d3d703e15eaed91cdb07eb40a0c8ec41db
Aditya Patel <adityapatel8169@gmail.com>
2025-01-16 19:36:53 +0530

    changed some text format

--- lms.py
+++ lms.py
@@ -19,15 +19,16 @@ class library:
     def returnbook(self):
         book=input("enter the bookname u want to return or donate: ")
         self.books.append(book)
-        print("ty for returning the book\n")
+        print("thank you for returning the book\n")
 lms=library(["django","flask","python","uiux","html","css"])
 while True:
-    print('''hey there welcome to lms
-      select any option from below:
-      1:for getting avl books list
-      2:for borrowing book
-      3:for returning book
-      4:to exit''')
+    print('''Hey there Welcome to my Library
+    
+Select any option from below:
+1:for getting avl books list
+2:for borrowing book
+3:for returning book
+4:to exit''')
     a=int(input("enter any option: "))
     if a==1:
         lms.avlbooks()
